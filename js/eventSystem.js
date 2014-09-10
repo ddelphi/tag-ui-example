428b3d283c052fba45d7c87d4be801570a4dee72
ddelphi <ddelphi@gmail.com>
2014-09-11 00:14:55 +0800

    First pages commit

---
+++ js/eventSystem.js
@@ -0,0 +1,10 @@
+(function() {
+
+var Backbone = window.Backbone
+var _ = window._
+var app = window.app
+
+// eventSystem are the Event object from Backbonejs framework
+app.eventSystem = _.clone(Backbone.Events)
+
+})()
